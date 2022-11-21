0dd2ce8ffaa9880bb1596e7090b77b37be106e32
Heon Soo Park <hspark220@gmail.com>
2022-11-21 08:14:39 -0500

    Add result message to show up on screen
    made result message to show up on screen using dom

--- assets/script.js
+++ assets/script.js
@@ -5,17 +5,23 @@
 //global variable, scores
 let playerScore = 0;
 let computerScore = 0;
+let resultTextString = '';
 
 //scoreboard initialization
 const scoreboard = document.querySelector('.scoreboard');
 const score = document.createElement('h1');
 scoreboard.appendChild(score);
 
-//picked options
+//picked options dom
 const optionPicked = document.querySelector('.options-picked');
 const playerOption = document.createElement('p');
 const computerOption = document.createElement('p');
 optionPicked.append(playerOption,computerOption);
+
+//results dom
+const result = document.querySelector('.results');
+const resultText = document.createElement('p');
+result.append(resultText);
 
 //returns a random choice for rps
 let getComputerChoice = () => {
@@ -40,51 +46,52 @@ let playRound = (computerSelection, playerSelection) => {
     if (playerSelection == "rock") {
         switch (computerSelection){
             case "rock":
-                console.log("It's a draw, Rock is Rock");
+                resultTextString = "It's a draw, Rock is Rock";
                 break;
             case "paper":
-                console.log("You lose! Paper beats Rock");
+                resultTextString = "You lose! Paper beats Rock";
                 computerScore++;
                 break;
             case "scissor":
-                console.log("You win! Rock beats Scissor");
+                resultTextString = "You win! Rock beats Scissor";
                 playerScore++;
                 break;
         }
     } else if (playerSelection == "paper") {
         switch(computerSelection) {
             case "rock":
-                console.log("You win! Paper beats Rock");
+                resultTextString = "You win! Paper beats Rock";
                 playerScore++;
                 break;
             case "paper":
-                console.log("It's a draw, Paper is Paper");
+                resultTextString = "It's a draw, Paper is Paper";
                 break;
             case "scissor":
-                console.log("You lose! Scissor beats Paper");
+                resultTextString = "You lose! Scissor beats Paper";
                 computerScore++;
                 break;
         }
     } else if (playerSelection == "scissor") {
         switch(computerSelection) {
             case "rock":
-                console.log("You lose! Rock beats Scissor");
+                resultTextString = "You lose! Rock beats Scissor";
                 computerScore++;
                 break;
             case "paper":
-                console.log("You win! Scissor beats Paper");
+                resultTextString = "You win! Scissor beats Paper";
                 playerScore++;
                 break;
             case "scissor":
-                console.log("It's a draw, Scissor is Scissor");
+                resultTextString = "It's a draw, Scissor is Scissor";
                 break;
         }
     } else {
-        console.log("invalid selection try again (make sure to input rock paper or scissor");
+        resultTextString = ("invalid selection try again (make sure to input rock paper or scissor");
     }
     score.textContent = `${playerScore} : ${computerScore}`;
     computerOption.textContent = `${computerSelection}`;
     playerOption.textContent = `${playerSelection}`;
+    resultText.textContent = `${resultTextString}`
 
     
 }
